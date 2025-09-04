d9918e7af6a1f150f0cc8c6ff2c9086f1839c0d5
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-09-04 20:08:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // PostgreSQL connection string
 const pool = new Pool({
   connectionString: "postgresql://postgres.ilnnyzpxwzyenvfecotq:regis.1@aws-1-eu-north-1.pooler.supabase.com:6543/postgres",
-//ssl: { rejectUnauthorized: false }
+ssl: { rejectUnauthorized: false }
 });
 async function checkDBConnection() {
   let client;
