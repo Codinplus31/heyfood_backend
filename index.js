ffdd423432e50acc1d41686c3f89f836bb4d89a9
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-09-05 15:12:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { Pool } = require("pg");
 const path = require("path");
 const restaurantTags = require("./tag.json");
 const restaurantDemoData = require("./restaurant.json");
-//require("dotenv").config();
-const serverless = require("serverless-http");
+require("dotenv").config();
+//const serverless = require("serverless-http");
 
 
 
@@ -217,8 +217,8 @@ const PORT = process.env.PORT || 5000;
 (async () => {
   await checkDBConnection();  // check DB first
   await initializeDB();       // initialize tables + demo data
- // app.listen(PORT, () => {
-  //  console.log(`🚀 Server running at http://localhost:${PORT}`);
- // });
+ app.listen(PORT, () => {
+ console.log(`🚀 Server running at http://localhost:${PORT}`);
+  });
 })();
-module.exports = serverless(app);
+//module.exports = serverless(app);
