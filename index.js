e73552c5942259ea493c2c5748dff35d7469426c
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-09-05 07:07:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
 const path = require("path");
-const restaurantTags = require("./tags.json");
+const restaurantTags = require("./tag.json");
 require("dotenv").config();
 
 
